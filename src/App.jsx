bdb19f72ba6f203960f066c13e9d22db2bd378ea
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-10-02 00:16:21 -0400

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,34 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+
+// story components
 import StoriesList from "./stories/StoriesList";
 import StoryDetail from "./stories/StoryDetail";
 import NewStoryForm from "./stories/NewStoryForm";
-import PageDetail from "./pages/PageDetail";
-import OptionForm from "./pages/OptionForm";
+import PageDetail from "./stories/PageDetail";
 
+/**
+ * App sets up all routes for the frontend.
+ * Each path points to a component that handles its logic.
+ */
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        {/* Home page */}
-        <Route index element={<p>Home page</p>} />
+        {/* Home page shows all stories */}
+        <Route index element={<StoriesList />} />
 
-        {/* Auth */}
+        {/* User auth routes */}
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
 
-        {/* Stories */}
-        <Route path="/stories" element={<StoriesList />} />
+        {/* Story routes */}
         <Route path="/stories/new" element={<NewStoryForm />} />
         <Route path="/stories/:id" element={<StoryDetail />} />
 
-        {/* Pages + Options */}
+        {/* Page routes (each story page with its options) */}
         <Route path="/pages/:id" element={<PageDetail />} />
-        <Route path="/pages/:id/options/new" element={<OptionForm />} />
       </Route>
     </Routes>
   );
